7f4e05439dc0dbf183b6e2233c60b385c90de2f7
Juan Carlos Tapia <juancarlostapiab.1@gmail.com>
2024-05-22 21:20:04 -0700

    Added AuthenticationService file and dependencies in pom

--- src/main/java/com/fcfm/pia/sevices/AuthenticacionService.java
+++ src/main/java/com/fcfm/pia/sevices/AuthenticacionService.java
@@ -1,4 +1,14 @@
 package com.fcfm.pia.sevices;
 
+import org.springframework.http.ResponseEntity;
+import com.fcfm.pia.DTO.MedicoDTO;
+import com.fcfm.pia.DTO.LoginResponse;
+import com.fcfm.pia.DTO.MedicoLoginDTO;
+import com.fcfm.pia.repository.entity.Medico;
+
+
 public interface AuthenticacionService {
+    public ResponseEntity<MedicoDTO> signup(Medico medico);
+
+    public ResponseEntity<LoginResponse> login(MedicoLoginDTO medicoLoginDTO);
 }
--- src/main/java/com/fcfm/pia/sevices/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/fcfm/pia/sevices/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,15 @@
 package com.fcfm.pia.sevices.impl;
 
+import org.springframework.stereotype.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthenticationServiceImpl {
 
 }
